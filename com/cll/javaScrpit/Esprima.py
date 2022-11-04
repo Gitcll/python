27e1618c507cc31b93ae79d3c8578b05acbdd6a0
GitCll <302701569qq@gmail.com>
2022-11-04 23:23:50 +0800

    js解析工具

--- com/cll/javaScrpit/Esprima.py
+++ com/cll/javaScrpit/Esprima.py
@@ -1,13 +1,49 @@
 import esprima
-from .visitor import NodeVisitor
+from com.cll.io.FileIO import *
 
-
+#js行解析 start
 program = 'const answer = 42'
 returnTokenizeStr = esprima.tokenize(program)
 returnParseScriptStr = esprima.parseScript(program)
-returnParseModuleStr = esprima.parseModule(program);
+returnParseModuleStr = esprima.parseModule(program)
+#js行解析 end
 
+#js片段解析 start
+str = "function _menuShow () {\
+        $('nav a').addClass('menu-active');\
+        $('.menu-bg').show();\
+        $('.menu-item').css({opacity: 0});\
+        TweenLite.to('.menu-container', 1, {padding: '0 40px'});\
+        TweenLite.to('.menu-bg', 1, {opacity: '0.92'});\
+        TweenMax.staggerTo('.menu-item', 0.5, {opacity: 1}, 0.3);\
+        _menuOn = true;\
+        $('.menu-bg').hover(function () {\
+            $('nav a').toggleClass('menu-close-hover');\
+        });\
+        }"
+returnTokenizeStr = esprima.tokenize(str)
+returnParseScriptStr = esprima.parseScript(str)
+returnParseModuleStr = esprima.parseModule(str)
+#js片段解析 end
 
-#print(returnTokenizeStr)
-#print(returnParseScriptStr)
+#读取js文件 start
+fileDirName = "D:\sql"
+arrayDate = []
+arrayDate = get_fileName(fileDirName, ".js")
+json_data_map = ''
+for jsFile in arrayDate:
+    with open(jsFile, 'r', encoding='utf-8') as f:
+        json_data_map = f.read()
+#读取js文件 end
 
+#解析js文件 start
+returnParseScriptStr = esprima.parseScript(json_data_map)
+#解析js文件 end
+
+print(returnParseScriptStr)
+
+#判断js类型 start
+for body in returnParseScriptStr.body:
+    if body.type == 'FunctionDeclaration':
+        print(body.type)
+#判断js类型 start
--- com/cll/javaScrpit/JsAst.py
+++ com/cll/javaScrpit/JsAst.py
@@ -1,24 +1,46 @@
 import js2py
 import json
+from com.cll.io.FileIO import *
 
 esprima = js2py.require('esprima')
 escodegen = js2py.require('escodegen')
 
-str = 'function onchangeHiddenMultiple(checkbox, checkboxHidden){\
-	//var allCheckChanges = document.getElementsByClassName(checkboxHidden);\
-	var input = document.getElementsByTagName("input");\
-	$.each(input,function(index,obj){\
-		var checkboxId = document.getElementById(checkbox+"_"+index);\
-		if(checkboxId != undefined && checkboxId != null){\
-			if(input[index].value == "true"){\
-				document.getElementById(checkbox+"_"+index).checked = true;\
-			}else{\
-				document.getElementById(checkbox+"_"+index).checked = false;\
-			}\
-		}\
-	});\
-}'
+#js片段解析 start
+str = "function _menuShow () {\
+        $('nav a').addClass('menu-active');\
+        $('.menu-bg').show();\
+        $('.menu-item').css({opacity: 0});\
+        TweenLite.to('.menu-container', 1, {padding: '0 40px'});\
+        TweenLite.to('.menu-bg', 1, {opacity: '0.92'});\
+        TweenMax.staggerTo('.menu-item', 0.5, {opacity: 1}, 0.3);\
+        _menuOn = true;\
+        $('.menu-bg').hover(function () {\
+            $('nav a').toggleClass('menu-close-hover');\
+        });\
+        }"
 tree = esprima.parse(str)
+#js片段解析 end
+
+#修改js属性值 start
+tree.body[0].body.body[0].expression.arguments[0].value = "5555"
+#修改js属性值 end
+
+#读取js文件 start
+fileDirName = "D:\sql"
+arrayDate = []
+arrayDate = get_fileName(fileDirName, ".js")
+json_data_map = ''
+for jsFile in arrayDate:
+    with open(jsFile, 'r', encoding='utf-8') as f:
+        json_data_map = f.read()
+#读取js文件 end
+
+#解析js文件 start
+tree = esprima.parse(json_data_map)
 print(json.dumps(tree.to_dict(), indent=4))
-tree.body
-print(esprima)
+#解析js文件解析 end
+
+#创建js start
+generate = escodegen.generate(tree)
+print(generate)
+#创建js end
