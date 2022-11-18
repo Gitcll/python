7f82b179e41b710553bb0410b80ec652b5c459aa
GitCll <302701569qq@gmail.com>
2022-11-19 00:01:11 +0800

    修改默认excel名称和sheet名称 设置文件夹名称为excel名称 设置index.html中class包名为sheet名

--- com/cll/coverageHtml/CoverageHtml.py
+++ com/cll/coverageHtml/CoverageHtml.py
@@ -7,8 +7,6 @@ import pandas as pd
 path = "C:\\Users\\30270\\HBuilderProjects"
 #查找当前文件下的第一层级文件夹
 fileDirectoryArr = get_directory(path)
-#e = 0代表生成excel顺序
-e = 0
 for fileDirectory in fileDirectoryArr:
     #初始化
     fileNameArr.clear()
@@ -16,12 +14,11 @@ for fileDirectory in fileDirectoryArr:
     fileNameArr = get_fileName(fileDirectory, ".html")
     #w = 0代表生成sheet顺序
     w = 0
+    excelName = fileDirectory[str(fileDirectory).rindex("\\")+1:]
     #创建以第一层级文件夹Excel文件
-    writer = pd.ExcelWriter("excel 样例" + str(e) + ".xlsx", mode='w', engine='openpyxl')
-    e += 1
+    writer = pd.ExcelWriter(excelName + ".xlsx", mode='w', engine='openpyxl')
     for file in fileNameArr:
         if str(file).endswith("index.html"):
-            print(file)
             #读取index.html文件,以数组的形式返回
             date = read_file_getArrayData(file)
             #解析当前index.html
@@ -32,9 +29,10 @@ for fileDirectory in fileDirectoryArr:
                 #初始化字典
                 dateArr11 = {}
                 #设置sheet默认标题行
-                dateArr11["biaotou"] = ['プロジェクト名', 'Javaファイル', 'カバー率']
+                dateArr11["biaotou"] = ['序号', 'プロジェクト名', 'Javaファイル', 'カバー率']
                 #i = 0标记dateArr11中key不能重复
                 i = 0
+                sheetName = ""
                 #tbody.select('tr')查找tbody下所有的tr标签
                 for tbodyChildren in tbody.select('tr'):
                     #使用正则匹配当前tr标签中存在百分比
@@ -43,6 +41,8 @@ for fileDirectory in fileDirectoryArr:
                     if res != None:
                         #初始化,存放数据
                         dateArr = []
+                        #添加序列
+                        dateArr.append(i)
                         #获取项目名<td id="a0"><a href="App.html" class="el_class">App</a></td>
                         dateArr.append(child_page.select('#breadcrumb a.el_bundle')[0].string)
                         #child_page.title.string获取包名
@@ -50,6 +50,8 @@ for fileDirectory in fileDirectoryArr:
                         dateArr.append(child_page.title.string + '.' + tbodyChildren.td.a.string)
                         #获取覆盖率百分比
                         dateArr.append(res.group())
+                        #设置sheet名字
+                        sheetName = child_page.title.string
                         #将数据存放入字典
                         dateArr11[child_page.title.string + '.' + tbodyChildren.td.a.string + str(i)] = dateArr
                         i += 1
@@ -58,7 +60,7 @@ for fileDirectory in fileDirectoryArr:
                 #T代表可以将数据横向排列,输出表格中
                 data = pd.DataFrame(dateArr11).T
                 #写入数据
-                data.to_excel(writer, sheet_name="这是第" + str(w) + "个sheet",index=False)
+                data.to_excel(writer, sheet_name=sheetName + str(w),index=False)
                 w += 1
     #保存excel,并关闭
     writer.save()
